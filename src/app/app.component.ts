dcd5d0064008ce7d1d9c05ded3d37dfd0f6bb0b4
Nicolás García Moreira <niicogarciia@MacBook-Air-de-Nicolas.local>
2026-02-13 19:46:43 +0100

    se añaden anuncios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'Precios Combustibles';
+
+  ngAfterViewInit(): void {
+    try {
+      ((window as any).adsbygoogle = (window as any).adsbygoogle || []).push({});
+    } catch (error) {
+      // ignore
+    }
+  }
 }
